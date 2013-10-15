02a05c69857fffd0745a8e5e9c13bab50cbde61e
Friedrich Lindenberg <friedrich@pudo.org>
2013-10-15 17:08:15 +0800

    Implement an angular browserid directive to enable login/logout.

---
+++ grano/static/js/directives/browserid.js
@@ -0,0 +1,44 @@
+grano.directive('browserid', function ($window, $http) {
+    return {
+        restrict: 'A',
+        link: function(scope, element, attrs) {
+            var gotAssertion = function(assertion) {
+                console.log(assertion);
+                if (assertion) {
+                    var data = 'assertion=' + assertion,
+                        req = $http.post('/api/1/browserid/login', data, {
+                            headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+                        });
+                    req.then(function(res, status, xhr) {
+                            return $window.location.reload(true);
+                        },
+                        function(res, status, xhr) {
+                            return alert("login failure: " + status);
+                        }
+                    );
+                }
+            };
+
+            var logoutCallback = function(event) {
+                var req = $http.post('/api/1/browserid/logout');
+                req.then(function() {
+                        return $window.location.reload(true);
+                    },
+                    function(res, status, xhr) {
+                        console.log(res);
+                        return alert("logout failure: " + status);
+                    });
+                return false;
+            };
+
+            element.bind('click', function() {
+                if (attrs.browserid==='login') {
+                    navigator.id.get(gotAssertion);
+                } else {
+                    navigator.id.logout(logoutCallback);
+                }
+                return false;
+            });
+        }
+    };
+});
