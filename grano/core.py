d40e52de6abcb3f747fdb7026fb0e770d46fef54
Friedrich Lindenberg <friedrich@pudo.org>
2013-10-16 08:32:55 +0200

    Begin depending on mongoengine for OO mongo

--- grano/core.py
+++ grano/core.py
@@ -1,12 +1,18 @@
 from flask import Flask
-from flask.ext.pymongo import PyMongo
 from flask.ext.assets import Environment
 
+from mongoengine import register_connection
 from grano import default_settings
 
 app = Flask(__name__)
 app.config.from_object(default_settings)
 app.config.from_envvar('GRANO_SETTINGS', silent=True)
 
-mongo = PyMongo(app)
 assets = Environment(app)
+
+register_connection('default',
+    app.config.get('MONGODB_DATABASE', 'grano'),
+    host=app.config.get('MONGODB_HOST', 'localhost'),
+    port=app.config.get('MONGODB_PORT', 27017),
+    username=app.config.get('MONGODB_USERNAME'),
+    password=app.config.get('MONGODB_PASSWORD'))
