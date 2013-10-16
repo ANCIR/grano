6cb88f923c99906c6be8747d08a03364a9fe1c96
Friedrich Lindenberg <friedrich@pudo.org>
2013-10-16 08:32:01 +0200

    Clean up management functions.

--- grano/manage.py
+++ grano/manage.py
@@ -1,16 +1,11 @@
 from flask.ext.script import Manager
 from flask.ext.assets import ManageAssets
 
-from grano.core import mongo, assets
+from grano.core import assets
 from grano.views import app
 
 manager = Manager(app)
 manager.add_command("assets", ManageAssets(assets))
-
-@manager.command
-def createdb():
-    """ Create the database entities. """
-    #db.create_all()
 
 if __name__ == "__main__":
     manager.run()
