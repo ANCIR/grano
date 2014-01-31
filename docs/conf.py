255f8d53a4cf3343a68cc5c932dbd3c80a327388
Friedrich Lindenberg <friedrich@pudo.org>
2014-01-31 10:06:18 +0100

    Redirect from README.

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'grano'
-copyright = u'2013, Friedrich Lindenberg'
+copyright = u'2014, Friedrich Lindenberg'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
