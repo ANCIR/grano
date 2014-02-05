25a37ea86e26a731608e4c2a810610c842d37d19
Friedrich Lindenberg <friedrich@pudo.org>
2014-02-05 21:38:24 +0100

    Remove use of yield_per which clashes with 'joined' load.

--- grano/service/indexer.py
+++ grano/service/indexer.py
@@ -13,16 +13,18 @@ log = logging.getLogger(__name__)
 
 def index_entities():
     """ Re-build an index for all enitites from scratch. """
-    for i, entity in enumerate(Entity.all().filter_by(same_as=None).yield_per(1000)):
+    for i, entity in enumerate(Entity.all().filter_by(same_as=None)):
         body = entities.to_index(entity)
+        
         if not 'name' in body:
             log.warn('No name: %s, skipping!', entity.id)
             continue
+    
         es.index(index=es_index, doc_type='entity', id=body.pop('id'), body=body)
-        #log.info('Indexing: %s', body.get('name'))
         if i > 0 and i % 1000 == 0:
             log.info("Indexed: %s entities", i)
             es.indices.refresh(index=es_index)
+        
     es.indices.refresh(index=es_index)
 
 
