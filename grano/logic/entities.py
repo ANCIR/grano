b53422e0b765992b32e60a96029b1718339f8d54
Friedrich Lindenberg <friedrich@pudo.org>
2014-02-20 16:54:58 +0100

    Duplicate schemata don't validate right.

--- grano/logic/entities.py
+++ grano/logic/entities.py
@@ -25,7 +25,7 @@ def validate(data):
     of the model, validation of entities has to happen in three steps. """
 
     # a bit hacky
-    data['schemata'] = data.get('schemata', []) + ['base']
+    data['schemata'] = set(data.get('schemata', []) + ['base'])
 
     validator = EntityBaseValidator()
     sane = validator.deserialize(data)
