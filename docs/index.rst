fa04692dddcd3faf6e6c46389577259f24e28435
Friedrich Lindenberg <friedrich@pudo.org>
2014-02-24 23:10:22 +0100

    Create a simple Angular application for the frontend.

--- docs/index.rst
+++ docs/index.rst
@@ -1,6 +1,3 @@
-.. dataset documentation master file, created by
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
 
 grano: facebook, for terrorists!
 =========================================
---
+++ grano/views/ui.py
@@ -0,0 +1,31 @@
+import os
+
+from flask import Blueprint, render_template, request
+from flask import redirect, make_response, url_for
+
+from grano import __version__
+from grano.lib.serialisation import jsonify
+from grano.core import app, url_for, app_name
+from grano.views.cache import validate_cache, disable_cache
+from grano.background import ping
+
+
+blueprint = Blueprint('ui', __name__)
+
+def angular_templates():
+    if app.config.get('ASSETS_DEBUG'):
+        return
+    partials_dir = os.path.join(app.static_folder, 'templates')
+    for (root, dirs, files) in os.walk(partials_dir):
+        for file_name in files:
+            file_path = os.path.join(root, file_name)
+            with open(file_path, 'rb') as fh:
+                file_name = file_path[len(partials_dir)+1:]
+                yield ('/static/templates/%s' % file_name,
+                       fh.read().decode('utf-8'))
+
+
+@blueprint.route(app.config.get('UI_PREFIX', '/'))
+def index(**kw):
+    return render_template('app.html',
+        angular_templates=angular_templates())
