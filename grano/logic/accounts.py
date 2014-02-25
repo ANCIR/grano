9fe0e4e71045bc8233fdebf4b14183647a0cb2e7
Friedrich Lindenberg <friedrich@pudo.org>
2014-02-25 14:22:16 +0100

    Have just a single account function.

--- grano/logic/accounts.py
+++ grano/logic/accounts.py
@@ -2,20 +2,17 @@ from grano.core import db
 from grano.model import Account
 
 
-def create(data):
-    account = Account()
-    account.github_id = data['id']
-    account.login = data['login']
+def create(data, account=None):
+    if account is None:
+        account = Account()
+        account.github_id = data.get('github_id')
+        account.twitter_id = data.get('twitter_id')
+        account.facebook_id = data.get('facebook_id')
+    account.login = data.get('login')
+    account.full_name = data.get('full_name')
     account.email = data.get('email')
     db.session.add(account)
     db.session.flush()
-    return account
-
-
-def update(account, data):
-    account.login = data['login']
-    account.email = data.get('email')
-    db.session.add(account)
     return account
 
 
@@ -30,11 +27,14 @@ def to_rest_index(account):
     return {
         'id': account.id,
         'login': account.login,
+        'full_name': account.full_name
         }
 
 def to_rest(account):
     data = to_rest_index(account)
     data['github_id'] = account.github_id
+    data['twitter_id'] = account.twitter_id
+    data['facebook_id'] = account.facebook_id
     data['created_at'] = account.created_at 
     data['updated_at'] = account.updated_at
     return data
