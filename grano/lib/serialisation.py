0e7dcfc58dca8ac3b2ff7f83f922e5edefdc1188
Friedrich Lindenberg <friedrich@pudo.org>
2014-04-04 23:39:29 +0200

    - debug.

--- grano/lib/serialisation.py
+++ grano/lib/serialisation.py
@@ -15,7 +15,6 @@ class JSONEncoder(json.JSONEncoder):
         super(JSONEncoder, self).__init__()
 
     def default(self, obj):
-        print obj
         if self.index and hasattr(obj, 'to_dict_index'):
             return obj.to_dict_index()
         if hasattr(obj, 'to_dict'):
