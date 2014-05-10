09bbdbcf78217aecfe2823be44313fccb88f5b47
Friedrich Lindenberg <friedrich@pudo.org>
2014-05-10 19:04:37 +0200

    pep8 imports code.

--- grano/logic/imports.py
+++ grano/logic/imports.py
@@ -17,9 +17,9 @@ class ImportBaseValidator(colander.MappingSchema):
     file = colander.SchemaNode(FileRef())
     project = colander.SchemaNode(ProjectRef())
     source_url = colander.SchemaNode(colander.String(),
-        empty=None, missing=None)
+                                     empty=None, missing=None)
     mode = colander.SchemaNode(colander.String(),
-        validator=colander.OneOf(MODES))
+                               validator=colander.OneOf(MODES))
 
 
 def make_importer(project, account, data):
@@ -29,7 +29,7 @@ def make_importer(project, account, data):
     validator = ImportBaseValidator()
     sane = validator.deserialize(data)
 
-    # TODO: validate mapping. 
+    # TODO: validate mapping.
     config = {
         'mode': sane.get('mode'),
         'file': sane.get('file').id,
@@ -38,7 +38,7 @@ def make_importer(project, account, data):
         'relation_schema': data.get('relation_schema')
     }
     pipeline = pipelines.create(project, 'import',
-        sane.get('file').file_name, config, account)
+                                sane.get('file').file_name, config, account)
     run_importer.delay(pipeline.id)
     return pipeline
 
@@ -54,7 +54,7 @@ def run_importer(pipeline_id):
     file_id = pipeline.config.get('file')
     file_ = File.by_id(file_id)
     if file_ is None:
-        piplines.log_error(pipeline, 'File object deleted: %s' % file_id)
+        pipeline.log_error(pipeline, 'File object deleted: %s' % file_id)
     elif mode == 'aliases':
         import_aliases(pipeline, file_.fh)
     else:
@@ -64,7 +64,7 @@ def run_importer(pipeline_id):
 
 
 def _row_source_url(pipeline, row):
-    """ Determine the best available source URL for the given 
+    """ Determine the best available source URL for the given
     row of data. """
     for k, v in pipeline.config.get('mapping', {}).items():
         if v.get('attribute') == '_source_url':
@@ -92,13 +92,14 @@ def import_aliases(pipeline, fh):
 
     for i, row in enumerate(importer):
         source_url = _row_source_url(pipeline, row)
+
         entities.apply_alias(pipeline.project, pipeline.author,
-            row.get(canonical_column),
-            row.get(alias_column), 
-            source_url=source_url)
+                             row.get(canonical_column),
+                             row.get(alias_column),
+                             source_url=source_url)
         
         if i % 100 == 0:
-            percentage = int((float(i) / max(1, len(importer)))*100)
+            percentage = int((float(i) / max(1, len(importer))) * 100)
             pipeline.percent_complete = percentage
             db.session.commit()
 
@@ -117,11 +118,11 @@ def _mapping_schemata(project, mapping, obj):
 
 
 def import_objects(pipeline, fh):
-    """ Import objects - either individual entities or relations 
-    and their involved entities (the target and source) - from a 
+    """ Import objects - either individual entities or relations
+    and their involved entities (the target and source) - from a
     CSV file. """
 
-    # Code is a bit ugly as this handles two cases at once: 
+    # Code is a bit ugly as this handles two cases at once:
     #  mode 'relations' where we import a source, target and relation
     #  mode 'entities' where we only import a single entity
 
@@ -136,18 +137,18 @@ def import_objects(pipeline, fh):
 
     importer = CSVImporter(fh)
     loader_ = loader.Loader(pipeline.project.slug, account=pipeline.author,
-        ignore_errors=False)
+                            ignore_errors=False)
 
     for i, row in enumerate(importer):
         try:
-            source_url = _row_source_url(pipeline, row)
+            url = _row_source_url(pipeline, row)
             rel_data = {}
-            source = loader_.make_entity(source_schemata, source_url=source_url)
-            target = loader_.make_entity(target_schemata, source_url=source_url)
-            entity = loader_.make_entity(entity_schemata, source_url=source_url)
+            source = loader_.make_entity(source_schemata, source_url=url)
+            target = loader_.make_entity(target_schemata, source_url=url)
+            entity = loader_.make_entity(entity_schemata, source_url=url)
 
             # Try to assign each column to the appropriate object in this
-            # loader. 
+            # loader.
             for column, spec in mapping.items():
                 attr = spec.get('attribute')
                 obj = spec.get('object')
@@ -166,13 +167,13 @@ def import_objects(pipeline, fh):
                     target.set(attr, value)
 
             # Relation can only be saved once the entities are available,
-            # hence we're storing the relation property values and now 
+            # hence we're storing the relation property values and now
             # making the whole thing.
             if mode == 'relations':
                 source.save()
                 target.save()
                 rel = loader_.make_relation(relation_schema, source,
-                    target, source_url=source_url)
+                                            target, source_url=url)
                 for k, v in rel_data.items():
                     rel.set(k, v)
                 rel.save()
@@ -181,11 +182,12 @@ def import_objects(pipeline, fh):
             
             # indicate progress, and commit every now and then.
             if i % 100 == 0:
-                percentage = int((float(i) / max(1, len(importer)))*100)
+                percentage = int((float(i) / max(1, len(importer))) * 100)
                 pipeline.percent_complete = percentage
                 loader_.persist()
         except Invalid, inv:
-            pipelines.log_warn(pipeline, unicode(inv), 'Invalid data', inv.as_dict())
+            pipelines.log_warn(pipeline, unicode(inv), 'Invalid data',
+                               inv.as_dict())
         except Exception, exc:
             pipelines.log_error(pipeline, unicode(exc), 'Error', {})
 
