2a8ea763d7376391b3054a6cdf061cdcdcd0fac2
Friedrich Lindenberg <friedrich@pudo.org>
2014-05-13 20:28:27 +0200

    next attempt at making a working deduper. man this is ugly.

--- grano/logic/entities.py
+++ grano/logic/entities.py
@@ -103,8 +103,20 @@ def delete(entity):
 def merge(orig, dest):
     """ Copy all properties and relations from one entity onto another, then
     mark the source entity as an ID alias for the destionation entity. """
+    if orig.id == dest.id:
+        return orig
 
-    dest_active = [p.name for p in dest.active_properties]
+    if dest.same_as == orig.id:
+        return orig
+    
+    if orig.same_as == dest.id:
+        return dest
+
+    if dest.same_as is not None:
+        # potential infinite recursion here.
+        resolved_dest = Entity.by_id(dest.same_as)
+        if resolved_dest is not None:
+            return merge(orig, resolved_dest)
     
     schemata, seen_schemata = list(), set()
     for schema in dest.schemata + orig.schemata:
@@ -115,6 +127,7 @@ def merge(orig, dest):
 
     dest.schemata = schemata
 
+    dest_active = [p.name for p in dest.active_properties]
     for prop in orig.properties:
         if prop.name in dest_active:
             prop.active = False
@@ -146,8 +159,22 @@ def apply_alias(project, author, canonical_name, alias_name, source_url=None):
     canonical = None
 
     # de-duplicate existing entities with the same name.
+    known_names = set()
     for existing in Entity.by_name_many(project, canonical_name):
-        if canonical is not None:
+
+        for prop in existing.properties:
+            if prop.name != 'name':
+                continue
+            known_names.add(prop.value)
+
+            # make sure the canonical name is actually active
+            # TODO: is this desirable?
+            if prop.value == canonical_name:
+                prop.active = True
+            else:
+                prop.active = False
+
+        if canonical is not None and canonical.id != existing.id:
             canonical = merge(existing, canonical)
         else:
             canonical = existing
@@ -155,23 +182,28 @@ def apply_alias(project, author, canonical_name, alias_name, source_url=None):
     schema = Schema.by_name(project, 'base')
     attribute = schema.get_attribute('name')
 
+    # Find aliases, i.e. entities with the alias name which are not
+    # the canonical entity.
     q = Entity.by_name_many(project, alias_name)
     if canonical is not None:
         q = q.filter(Entity.id != canonical.id)
     aliases = q.all()
 
+    # If there are no existing aliases with that name, add the alias
+    # name to the canonical entity.
     if not len(aliases) and canonical is not None:
-        data = {
-            'value': alias_name,
-            'schema': schema,
-            'attribute': attribute,
-            'active': False,
-            'name': 'name',
-            'source_url': source_url
-        }
-        properties_logic.save(canonical, data)
-        _entity_changed.delay(canonical.id, 'update')
-        return log.info("Alias: %s -> %s", alias_name, canonical_name)
+        if alias_name not in known_names:
+            data = {
+                'value': alias_name,
+                'schema': schema,
+                'attribute': attribute,
+                'active': False,
+                'name': 'name',
+                'source_url': source_url
+            }
+            properties_logic.save(canonical, data)
+            _entity_changed.delay(canonical.id, 'update')
+        log.info("Alias: %s -> %s", alias_name, canonical_name)
 
     for alias in aliases:
         if canonical is None:
@@ -191,4 +223,5 @@ def apply_alias(project, author, canonical_name, alias_name, source_url=None):
             # Merge two existing entities, declare one as "same_as"
             merge(alias, canonical)
             log.info("Mapped: %s -> %s", alias.id, canonical.id)
-            continue
+
+    db.session.commit()
