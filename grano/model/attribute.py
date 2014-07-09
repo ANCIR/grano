fc663314bf2ada5a77d99ded3d4b9f21c30a6fbe
Friedrich Lindenberg <friedrich@pudo.org>
2014-07-09 14:31:31 +0200

    pep8 attribute and pipeline model.

--- grano/model/attribute.py
+++ grano/model/attribute.py
@@ -1,13 +1,12 @@
-from grano.core import db, url_for
+from grano.core import db
 from grano.model.common import IntBase
-from grano.model.util import slugify_column
 
 
 class Attribute(db.Model, IntBase):
     __tablename__ = 'grano_attribute'
 
     DATATYPES = {
-        'string': 'value_string', 
+        'string': 'value_string',
         'integer': 'value_integer',
         'float': 'value_float',
         'datetime': 'value_datetime',
@@ -19,16 +18,15 @@ class Attribute(db.Model, IntBase):
     description = db.Column(db.Unicode())
     hidden = db.Column(db.Boolean())
     datatype = db.Column(db.Unicode())
-    
+
     schema_id = db.Column(db.Integer, db.ForeignKey('grano_schema.id'))
     properties = db.relationship('Property', backref='attribute',
-        cascade='all, delete, delete-orphan', lazy='dynamic')
-
+                                 cascade='all, delete, delete-orphan',
+                                 lazy='dynamic')
 
     @property
     def value_column(self):
         return self.DATATYPES.get(self.datatype)
-
 
     @classmethod
     def all_named(cls, name):
@@ -36,14 +34,12 @@ class Attribute(db.Model, IntBase):
         q = q.filter_by(name=name)
         return q.all()
 
-
     @classmethod
     def by_schema_and_name(cls, schema, name):
         q = db.session.query(cls)
         q = q.filter_by(schema=schema)
         q = q.filter_by(name=name)
         return q.first()
-
 
     def to_index(self):
         return {
