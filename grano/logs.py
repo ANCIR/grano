b3788cc2cf877762d45ea560173f0e7147aaf599
Friedrich Lindenberg <friedrich@pudo.org>
2014-07-20 00:56:59 +0200

    move sa mention out of module base

--- grano/logs.py
+++ grano/logs.py
@@ -2,6 +2,14 @@ import logging
 
 logging.basicConfig(level=logging.DEBUG)
 
+
+# shut up useless SA warning:
+import warnings;
+warnings.filterwarnings('ignore', 'Unicode type received non-unicode bind param value.')
+from sqlalchemy.exc import SAWarning
+warnings.filterwarnings('ignore', category=SAWarning)
+
+# specific loggers
 requests_log = logging.getLogger("requests")
 requests_log.setLevel(logging.WARNING)
 
