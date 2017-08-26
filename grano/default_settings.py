0dd9a3851b2893847595bad454efce544025c83a
David Lemayian <dlemayian@gmail.com>
2017-08-26 21:01:33 +0200

    Add the PREFERRED_URL_SCHEME.

--- grano/default_settings.py
+++ grano/default_settings.py
@@ -16,6 +16,9 @@ CACHE_AGE = 84600
 SQLALCHEMY_DATABASE_URI = os.getenv("GRANO_DATABASE", "postgresql://grano:grano@postgres/grano")
 APP_NAME = CELERY_APP_NAME = ES_INDEX = os.getenv("GRANO_APP_NAME", "grano")
 
+# Force HTTPS here:
+PREFERRED_URL_SCHEME = env.get('GRANO_URL_SCHEME', 'http')
+
 # You need to create an application on GitHub which can be used
 # for OAuth sign-in.
 
